57201b58123f8bad9c7c99122b9e7223f0b751ce
Bukhari M <bukharim96@gmail.com>
2019-01-27 16:10:13 +0200

    Ready for deploy

---
+++ documate/website/precache-manifest.c383f397f209627943239c6ae5532fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ddb4f15c89ea64e6f2c",
+    "url": "/static/js/main.5ddb4f15.chunk.js"
+  },
+  {
+    "revision": "6a5c2bbf5471b930cf28",
+    "url": "/static/js/1.6a5c2bbf.chunk.js"
+  },
+  {
+    "revision": "5ddb4f15c89ea64e6f2c",
+    "url": "/static/css/main.b5529876.chunk.css"
+  },
+  {
+    "revision": "6a5c2bbf5471b930cf28",
+    "url": "/static/css/1.4cfa5170.chunk.css"
+  },
+  {
+    "revision": "698264663e4e0c1d4fd125262fd4e082",
+    "url": "/index.html"
+  }
+];
